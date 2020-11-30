8336aa7a9f14e8b9f665eb439a13d19558aa08fb
ioannispol <giannis.polimenis@gmail.com>
2020-11-30 19:41:01 +0000

    misspelled std

--- src/Inventory.cpp
+++ src/Inventory.cpp
@@ -1,16 +1,17 @@
 #include "../Headers/Inventory.h"
 #include <vector>
+#include<bits/stdc++.h> 
 
 void Inventory::AddBook(Book book)
 {
     Inventory::Books.push_back(book);
 }
 
-Book Inventory::FindBookByTitle(srd::string title)
+Book Inventory::FindBookByTitle(std::string title)
 {
     if (std::find(Inventory::Books.begin(), Inventory::Books.end(), Book(0, title, "")) != Inventory::Books.end())
     {
-        
+
     }
 
     return Book(1, "", "");
